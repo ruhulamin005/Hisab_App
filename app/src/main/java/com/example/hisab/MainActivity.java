7286dfc7d907a7bc1fccade46e6256dc7da14547
Ruhul Amin <ruhul005@users.noreply.github.com>
2020-04-25 01:52:09 +0600

    new activity for form

--- app/src/main/java/com/example/hisab/MainActivity.java
+++ app/src/main/java/com/example/hisab/MainActivity.java
@@ -6,16 +6,22 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //initializing button and webview
         webView = (WebView) findViewById(R.id.webview);
+        button = (Button) findViewById(R.id.button);
+
+
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://bit.ly/2Y2zpbm");
 
